ea1ddbbf71d55360bcd8e2b63dac343af1d0ec73
Jorge Adolfo Ramírez Uresti <juresti@tec.mx>
2022-09-26 14:50:40 -0500

    Create ejListas2.py

---
+++ Lunes 26 Sep - Listas/ejListas2.py
@@ -0,0 +1,15 @@
+def mayores30(listaNom,listaEdades):
+    cuantos = len(listaNom)
+    salida = []
+    for pos in range(cuantos):
+        if (listaEdades[pos] > 30):
+            salida.append(listaNom[pos])
+    return salida
+
+def altaEmpleados(numPer):
+    """Da de alta el nombre, edad, antiguedad y salario del numero de personas
+    que se le pase como parámetro
+    Regresa una lista con una tupla con la información de cada persona"""
+    TO BE CONTINUED...
+    
+    
